8ea08190e8d0280cf09e2012452f95049a5c20b9
cuong <cuongtm2012@gmail.com>
2018-06-21 16:30:11 +0700

    1st

---
+++ src/main/java/app/mainApp.java
@@ -0,0 +1,13 @@
+package app;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void readFile(String path){
+		
+	}
+}
